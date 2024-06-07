c2faba3a180dd91612aff0b4f1e841decc00612b
maskudo <manujkarki101@gmail.com>
2024-06-08 00:03:59 +0545

    feat: pi config

---
+++ pizero/flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+    with pkgs; {
+      devShells.${system}.default = mkShell {
+        buildInputs = [ansible act];
+      };
+    };
+}
