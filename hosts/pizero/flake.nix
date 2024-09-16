6682f1213dcec89c43dee290f4f9f01a200ab617
maskudo <manujkarki101@gmail.com>
2024-09-16 22:39:15 +0545

    fix: ufw into its own role, plus enable ssh before enabling ufw

--- hosts/pizero/flake.nix
+++ hosts/pizero/flake.nix
@@ -2,10 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
+  outputs = {nixpkgs, ...}: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in
--- hosts/pizero/run.sh
+++ hosts/pizero/run.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i 192.168.1.74, -K -u pizero local.yml
+ansible-playbook -i pi.manujkarki.com.np, -K -u pizero local.yml
