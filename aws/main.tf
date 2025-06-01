911e438e3454be63029dccb72893acf8be22f4fa
maskudo <manujkarki101@gmail.com>
2025-06-01 17:44:16 +0545

    feat: aws infra for paperless

--- aws/main.tf
+++ aws/main.tf
@@ -1,28 +1,53 @@
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
+
 }
 
-resource "aws_instance" "web-server" {
-  ami                         = "ami-0e35ddab05955cf57"
-  instance_type               = "t2.micro"
-  key_name                    = aws_key_pair.web-server-key-pair.key_name
-  vpc_security_group_ids      = [aws_security_group.web-server-sg.id]
-  user_data                   = <<-EOF
-    #!/usr/bin/env bash
-    echo "hello world" > index.html
-    nohup busybox httpd -f -p 8080 &
-  EOF
-  user_data_replace_on_change = true
+
+resource "aws_vpc" "paperless" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
   tags = {
-    Name = "ssh-machine"
+    Name = "paperless"
   }
 }
 
-resource "aws_security_group" "web-server-sg" {
-  name = "web-server-sg"
+resource "aws_subnet" "paperless-subnet" {
+  vpc_id                  = aws_vpc.paperless.id
+  cidr_block              = "10.0.1.0/28"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "paperless-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "paperless-igw" {
+  vpc_id = aws_vpc.paperless.id
+  tags = {
+    Name = "paperless-igw"
+  }
+}
+
+resource "aws_route_table" "paperless-rt" {
+  vpc_id = aws_vpc.paperless.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.paperless-igw.id
+  }
+}
+
+resource "aws_route_table_association" "paperless-rta" {
+  subnet_id      = aws_subnet.paperless-subnet.id
+  route_table_id = aws_route_table.paperless-rt.id
+}
+
+resource "aws_security_group" "paperless-sg" {
+  vpc_id = aws_vpc.paperless.id
+  name   = "paperless-sg"
   ingress {
-    from_port   = 8080
+    from_port   = 8000
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
@@ -33,6 +58,27 @@ resource "aws_security_group" "web-server-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "paperless-sg"
+  }
+}
+
+
+resource "aws_instance" "paperless" {
+  ami                    = "ami-0e35ddab05955cf57"
+  subnet_id              = aws_subnet.paperless-subnet.id
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.web-server-key-pair.key_name
+  vpc_security_group_ids = [aws_security_group.paperless-sg.id]
+  tags = {
+    Name = "paperless-machine"
+  }
 }
 
 resource "aws_key_pair" "web-server-key-pair" {
@@ -40,7 +86,7 @@ resource "aws_key_pair" "web-server-key-pair" {
   public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ2bl3yHCcGK2dvde0CYmWmcYWiBSju3OuVyj4O9afJp manujkarki101@gmail.com"
 }
 
-output "web-server-ip" {
-  value       = aws_instance.web-server.public_ip
+output "paperless-ip" {
+  value       = aws_instance.paperless.public_ip
   description = "IP address of web-server"
 }
