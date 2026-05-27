73f80ade342aeffc9a88d3eb7069da6ccd998f57
maskudo <manujkarki101@gmail.com>
2026-05-27 21:49:39 +0545

    fix: kube prom stack issue, update flake for darwin

--- flake.nix
+++ flake.nix
@@ -2,22 +2,39 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
+
   outputs =
     { nixpkgs, ... }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs systems (
+          system:
+          f {
+            pkgs = import nixpkgs {
+              inherit system;
+            };
+          }
+        );
     in
     {
-      devShells.${system}.default =
-        with pkgs;
-        mkShell {
-          buildInputs = [
-            age
-            sops
-            ssh-to-age
-            argocd
-          ];
-        };
+      devShells = forAllSystems (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              age
+              sops
+              ssh-to-age
+              argocd
+            ];
+          };
+        }
+      );
     };
 }
